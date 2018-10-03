02163be4e6ae166926cf593b50139863a92218df
unknown <wnzk413@gmail.com>
2018-10-03 21:33:07 +0900

    SQLite 끝

--- app/src/main/java/com/example/sy/sqlitetest/MainActivity.java
+++ app/src/main/java/com/example/sy/sqlitetest/MainActivity.java
@@ -47,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
